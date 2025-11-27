9d6b8dae818a30cff82cb8a76d6a84d7344cf52b
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-11-27 21:16:16 +0530

    Add files via upload

---
+++ BinaryTreesYT.java
@@ -0,0 +1,81 @@
+package Tree;
+
+public class BinaryTreesYT {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+
+            if(nodes[idx] == -1){
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void preOrder(Node root){
+
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void postOrder(Node root){
+        if (root == null) {
+
+            return ;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " " );
+    }
+
+    public static void InOrder(Node root){
+        if(root == null){
+            return;
+        }
+
+        InOrder(root.left);
+        System.out.print(root.data + " ");
+        InOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+        BinaryTree tree = new BinaryTree();
+
+        // ✅ store the returned root
+        Node root = tree.buildTree(nodes);
+
+        // Now you can print root
+        System.out.println(root.data);
+        System.out.println("PreOrder:");
+        preOrder(root);
+        System.out.println("\nPostOrder:");// Output: 1
+        postOrder(root);
+        System.out.println("\nInOrder:");
+        InOrder(root);
+    }
+}
